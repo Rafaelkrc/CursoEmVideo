a6da1d5af223a5678e0637e09cebde3b5b5a9d65
Rafael Rosso Coelho <rafaelkrc@hotmail.com>
2022-06-24 19:37:17 +0200

    atualizacao

--- ex033.py
+++ ex033.py
@@ -2,7 +2,7 @@ a = int(input('Digite o primeiro numero: '))
 b = int(input('Digite o segundo numero: '))
 c = int(input('Digite o terceiro numero: '))
 maior = a
-if (b>a and b>c):
+if (b > a and b > c):
     maior = b
 if (c>a and c>b):
     maior = c
